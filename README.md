cb14413b06b3476fa6f48f7ac6e7188597b1514f
Kolosok86 <kolosok86@MacBook-Air-Vladislav.local>
2021-08-14 15:00:44 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,95 @@
+# Underlords GC for Node.js
+
+This module provides a very flexible interface for interacting with the [Underlords](http://store.steampowered.com/app/1046930)
+Game Coordinator. It's designed to work with a
+[node-steam-user SteamUser](https://github.com/DoctorMcKay/node-steam-user) instance.
+
+This is based off of [node-tf2](https://github.com/DoctorMcKay/node-tf2).
+
+**You will need node-steam-user v4.2.0 or later and Node.js v8 or later.**
+
+# Setup
+
+First, install it from npm:
+
+	$ npm install dota-underlords
+
+Require the module and call its constructor with your SteamUser instance:
+
+```js
+const SteamUser = require('steam-user');
+const Underlords = require('dota-underlords');
+
+let user = new SteamUser();
+let underlords = new Underlords(user);
+```
+
+To initialize your GC connection, just launch Underlords via SteamUser normally:
+
+```js
+client.gamesPlayed([1046930]);
+```
+
+Underlords will emit a `connectedToGC` event when the game coordinator connection has been successfully
+established. You shouldn't try to do anything before you receive that event.
+
+# Enums
+
+There are some enums that are used by various methods and events. You can find them in `enums.js`.
+
+# Properties
+
+There are a few useful read-only properties available to you.
+
+### haveGCSession
+
+`true` if we're currently connected to the GC, `false` otherwise. You should only call methods when we have an active GC session.
+
+# Methods
+
+### Constructor(steamClient)
+
+When instantiating your underlords instance, you need to pass your active Steam.SteamClient instance as the sole parameter, as shown here:
+
+```js
+var underlords = new Underlords(steamClient);
+```
+
+### requestProfile(steamid[, callback])
+- `steamid` - The numeric SteamID of the Steam account to pull profile data for.
+- `callback` - Called if all parameters are valid when Steam responds to us.
+
+### requestMatches(steamid[, callback])
+- `steamid` - The numeric SteamID of the Steam account to pull profile data for.
+- `rows` - The number of matches.
+- `callback` - Called if all parameters are valid when Steam responds to us.
+
+# Events
+
+### connectedToGC
+
+Emitted when a GC connection is established. You shouldn't use any methods before you receive this. Note that this may be received (after it's first emitted) without any disconnectedFromGC event being emitted. In this case, the GC simply restarted.
+
+### disconnectedFromGC
+- `reason` - A value from the `GCConnectionStatus` enum
+
+Emitted when we're disconnected from the GC for any reason. underlords will automatically try to reconnect and will emit `connectedToGC` when reconnected.
+
+Example usage:
+
+```js
+const Underlords = require('dota-underlords');
+let underlordsClient = new Underlords(steamUser);
+
+underlordsClient.on('disconnectedFromGC', (reason) => {
+    if (reason == underlordsClient.GCConnectionStatus.GC_GOING_DOWN) {
+        console.log('GC going down');    
+    }
+});
+```
+
+### connectionStatus
+- `status` - A value from the `GCConnectionStatus` enum
+- `data` - The raw data that was received
+
+Emitted when we receive the status of our connection to the GC. Exactly when this is emitted is currently unknown. **This may be removed in the future.**
---
+++ handlers.js
@@ -0,0 +1,106 @@
+const ByteBuffer = require('bytebuffer');
+const Long = require('long');
+const SteamID = require('steamid');
+
+const Underlords = require('./index.js');
+const Language = require('./language.js');
+const Protos = require('./protobufs/generated/_load.js');
+
+let handlers = Underlords.prototype._handlers;
+
+// ClientWelcome and ClientConnectionStatus
+handlers[Language.ClientWelcome] = function(body) {
+	let proto = decodeProto(Protos.CMsgClientWelcome, body);
+
+	this.emit('debug', "GC connection established");
+	this.haveGCSession = true;
+	clearTimeout(this._helloTimer);
+	this._helloTimer = null;
+	this._helloTimerMs = 1000;
+	this.emit('connectedToGC');
+};
+
+handlers[Language.ClientConnectionStatus] = function(body) {
+	let proto = decodeProto(Protos.CMsgConnectionStatus, body);
+	this.emit('connectionStatus', proto.status, proto);
+
+	let statusStr = proto.status;
+	for (let i in Underlords.GCConnectionStatus) {
+		if (Underlords.GCConnectionStatus.hasOwnProperty(i) && Underlords.GCConnectionStatus[i] == proto.status) {
+			statusStr = i;
+		}
+	}
+
+	this.emit('debug', "Connection status: " + statusStr + " (" + proto.status + "); have session: " + (this.haveGCSession ? 'yes' : 'no'));
+
+	if (proto.status != Underlords.GCConnectionStatus.HAVE_SESSION && this.haveGCSession) {
+		this.emit('disconnectedFromGC', proto.status);
+		this.haveGCSession = false;
+		this._connect(); // Try to reconnect
+	}
+};
+
+// PlayersProfile
+handlers[Language.GetProfileResponse] = function(body){
+	let proto = decodeProto(Protos.CMsgClientToGCGetProfileResponse, body);
+	this.emit('playersProfile', proto);
+};
+
+// MatchHistory
+handlers[Language.GetMatchHistoryResponse] = function(body){
+	let proto = decodeProto(Protos.CMsgClientToGCGetMatchHistoryResponse, body);
+	this.emit('matchList', proto);
+}
+
+function decodeProto(proto, encoded) {
+	if (ByteBuffer.isByteBuffer(encoded)) {
+		encoded = encoded.toBuffer();
+	}
+
+	let decoded = proto.decode(encoded);
+	let objNoDefaults = proto.toObject(decoded, {"longs": String});
+	let objWithDefaults = proto.toObject(decoded, {"defaults": true, "longs": String});
+	return replaceDefaults(objNoDefaults, objWithDefaults);
+
+	function replaceDefaults(noDefaults, withDefaults) {
+		if (Array.isArray(withDefaults)) {
+			return withDefaults.map((val, idx) => replaceDefaults(noDefaults[idx], val));
+		}
+
+		for (let i in withDefaults) {
+			if (!withDefaults.hasOwnProperty(i)) {
+				continue;
+			}
+
+			if (withDefaults[i] && typeof withDefaults[i] === 'object' && !Buffer.isBuffer(withDefaults[i])) {
+				// Covers both object and array cases, both of which will work
+				// Won't replace empty arrays, but that's desired behavior
+				withDefaults[i] = replaceDefaults(noDefaults[i], withDefaults[i]);
+			} else if (typeof noDefaults[i] === 'undefined' && isReplaceableDefaultValue(withDefaults[i])) {
+				withDefaults[i] = null;
+			}
+		}
+
+		return withDefaults;
+	}
+
+	function isReplaceableDefaultValue(val) {
+		if (Buffer.isBuffer(val) && val.length == 0) {
+			// empty buffer is replaceable
+			return true;
+		}
+
+		if (Array.isArray(val)) {
+			// empty array is not replaceable (empty repeated fields)
+			return false;
+		}
+
+		if (val === '0') {
+			// Zero as a string is replaceable (64-bit integer)
+			return true;
+		}
+
+		// Anything falsy is true
+		return !val;
+	}
+}

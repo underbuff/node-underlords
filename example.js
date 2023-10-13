29d3b483e8b3b1949a3aa41a8c3c515247e309d9
Oleg Safonov <osafonov@ibagroup.eu>
2023-10-13 16:55:41 +0200

    feat: add example.js

---
+++ example.js
@@ -0,0 +1,38 @@
+const SteamUser = require('steam-user');
+const Underlords = require('./');
+const acc = require('./account.json')
+
+let client = new SteamUser();
+let underlordsClient = new Underlords(client);
+
+client.logOn({
+    accountName: acc.login,
+    password: acc.password
+});
+
+const steamIdMain = '76561199218271603'
+
+client.on('loggedOn', function(details) {
+    console.log('Logged into Steam as ' + client.steamID.getSteam3RenderedID());
+    client.setPersona(SteamUser.EPersonaState.Online, 'Underbuff Bot');
+    client.gamesPlayed(1046930);
+});
+
+underlordsClient.on('connectedToGC', (...args) => {
+    console.log('connected to GC', args)
+
+    underlordsClient.requestMatches({
+        steamid: steamIdMain,
+        rows: 2
+    }, console.log);
+
+    underlordsClient.requestProfile({ steamid: steamIdMain }, console.log)
+
+    underlordsClient.requestMatch({ match_id: 129273950 }, res => {
+        console.log(res)
+    })
+    underlordsClient.getFriendRanks(console.log)
+    underlordsClient.spectateUser({ account_id: 1258005875 }, console.log)
+});
+
+underlordsClient.on('debug', console.log)
